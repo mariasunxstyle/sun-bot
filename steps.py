b2106c652698009102b458b9bcd998c3647c5d43
mariasunxstyle <tohmian@mail.ru>
2025-05-21 08:18:28 +0300

    Add files via upload

--- steps.py
+++ steps.py
@@ -3,8 +3,21 @@ steps = [
     {
         "step": 1,
         "positions": [
-            {"name": "Лицом вверх", "duration_min": 0.083},  # 5 сек
-            {"name": "На животе", "duration_min": 0.083},   # 5 сек
+            {"name": "Лицом вверх", "duration_min": 0.083},
+            {"name": "На животе", "duration_min": 0.083},
+            {"name": "Левый бок", "duration_min": 0.083},
+            {"name": "Правый бок", "duration_min": 0.083},
+            {"name": "В тени", "duration_min": 0.083}
+        ]
+    },
+    {
+        "step": 2,
+        "positions": [
+            {"name": "Лицом вверх", "duration_min": 0.083},
+            {"name": "На животе", "duration_min": 0.083},
+            {"name": "Левый бок", "duration_min": 0.083},
+            {"name": "Правый бок", "duration_min": 0.083},
+            {"name": "В тени", "duration_min": 0.083}
         ]
     }
 ]
