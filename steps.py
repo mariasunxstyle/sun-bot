2b49a3f489d4cb35b7d22c5a6c4422a11dc177ba
mariasunxstyle <tohmian@mail.ru>
2025-05-23 12:33:29 +0300

    Add files via upload

--- steps.py
+++ steps.py
@@ -1,14 +1,14 @@
-# steps.py
 steps = [
-    {
-        "step": 1,
-        "positions": [
-            {"name": "Лицом вверх", "duration_min": 2},
-            {"name": "На животе", "duration_min": 2},
-            {"name": "Левый бок", "duration_min": 1},
-            {"name": "Правый бок", "duration_min": 1},
-            {"name": "В тени", "duration_min": 2},
-        ]
-    },
-    # остальные шаги по аналогии
+    {'step': 1, 'duration_min': 8},
+    {'step': 2, 'duration_min': 9},
+    {'step': 3, 'duration_min': 10},
+    {'step': 4, 'duration_min': 12},
+    {'step': 5, 'duration_min': 14},
+    {'step': 6, 'duration_min': 16},
+    {'step': 7, 'duration_min': 18},
+    {'step': 8, 'duration_min': 20},
+    {'step': 9, 'duration_min': 25},
+    {'step': 10, 'duration_min': 30},
+    {'step': 11, 'duration_min': 45},
+    {'step': 12, 'duration_min': 150},
 ]
